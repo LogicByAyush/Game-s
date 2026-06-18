a9969b9d45143e460144d6ff55ab6758cda0be5e
Ayush Gupta <uhbayush@gmail.com>
2026-06-18 11:50:31 +0530

    Updated guessing_game

--- guessing_game.py
+++ guessing_game.py
@@ -3,21 +3,25 @@ import random
 while True:
 
     print("\nWelcome to the Number Guessing Game!")
-    level = input("Choose level (easy/medium/hard): ").lower()
     move = 10
+    level = input("Choose level (easy/medium/hard): ").lower()
 
     if level == "easy":
         secret = random.randint(1, 50)
+        print("Guess the number between 1 to 50")
     elif level == "medium":
         secret = random.randint(1, 100)
+        print("Guess the number between 1 to 100")
     elif level == "hard":
         secret = random.randint(1, 500)
+        print("Guess the number between 1 to 500")
     else:
         print("Invalid level!")
         continue
 
     while move > 0:
         guess = int(input(f"\nYou have {move} moves remaining.\nEnter your guess: "))
+
         if guess < secret:
             print("Too low!")
         elif guess > secret:
@@ -25,6 +29,7 @@ while True:
         else:
             print("🎉 Congratulations! You guessed it.")
             break
+
         move -= 1
 
     if move == 0:
